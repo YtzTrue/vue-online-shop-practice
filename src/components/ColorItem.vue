76928aeefb2b453f7fa3442ff497514e8bc500ed
Yuri <yuretztrue@gmail.com>
2022-08-25 12:26:51 +0300

    components ProductFilter, ColorItem and BasePagination added

---
+++ src/components/ColorItem.vue
@@ -0,0 +1,20 @@
+<!-- eslint-disable max-len -->
+<!-- eslint-disable vuejs-accessibility/label-has-for -->
+<template>
+  <li class="colors__item">
+    <label class="colors__label">
+      <input class="colors__radio sr-only" type="radio" name="color" :value="color"
+        v-on:input="$emit('input', $event.target.value)">
+      <span class="colors__value" :style="{ backgroundColor: color }">
+      </span>
+    </label>
+  </li>
+
+</template>
+
+<script>
+
+export default {
+  props: ['color'],
+};
+</script>
--- src/data.js
+++
@@ -1,5 +0,0 @@
-const data = {
-  messageFirst: 'Hello Vue',
-  messageLast: 'Go on',
-};
-export default data;
